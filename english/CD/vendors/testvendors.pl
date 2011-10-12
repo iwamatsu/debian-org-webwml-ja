54674446de6a46f6f24d6220d0ee53f0891fac8d
webwml sync agent <kmuto_webwml@kmuto.jp>
2011-10-13 08:12:41 +0900

    [auto] sync with cvs.

--- english/CD/vendors/testvendors.pl
+++ english/CD/vendors/testvendors.pl
@@ -1,10 +1,20 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 # testvendors.pl - My evil vendor testing script
 # You will need wget for this baby to work.
 # Is the script evil or the vendors? Let the script decide
 # Written by Craig Small <csmall@debian.org> 
 # Copyright 2000 SPI Inc, released under the GPL if anyone would bother with it
 
+sub sanitize (@)
+{
+# Sanitize characters in the URL with their encoded entities
+    my ($url) = @_;
+    $url =~ s/&amp;/&/g;
+    $url =~ s/'/%27/g;
+    $url =~ s/ /%20/g;
+    $url =~ s/!/%21/g;
+    return $url;
+}
 sub test_vendor(@)
 {
     ($name, $url, $deburl,$email) = @_;
@@ -20,7 +30,8 @@ sub test_vendor(@)
         print "E: Vendor \"$name\" <$email> doesn't have a webpage!\n";
     } else {
         print "N: Checking $url\n";
-        $vendpage = `wget -t 1 -O/dev/null $url 2>&1`;
+        my $turl = sanitize($url);
+        $vendpage = `wget -t 1 -O/dev/null '$turl' 2>&1`;
         if ($vendpage =~ /: Host not found./)
         {
             print "E: Vendor \"$name\" <$email> cannot look up web page $url\n";
@@ -33,7 +44,8 @@ sub test_vendor(@)
         print "W: Vendor \"$name\" <$email> doesn't have a Debian webpage.\n";
     } else {
         print "N: Checking $deburl\n";
-        $vendpage = `wget -t 1 -O- $deburl 2>&1`;
+        my $turl = sanitize($deburl);
+        $vendpage = `wget -t 1 -O- '$turl' 2>&1`;
         if ($vendpage =~ /: Host not found./)
         {
             print "E: Vendor \"$name\" <$email> cannot look up web page $deburl\n";
@@ -48,12 +60,30 @@ sub test_vendor(@)
 
 }
 
-$filename = $ARGV[0];
-if ($filename eq "") 
+sub usage
 {
-    print "Usage $0 <filename>\n";
-    exit
+    print "Usage $0 [-dh] <filename>\n";
+    exit 1;
 }
+
+our($opt_d, $opt_h);
+my $debug = 0;
+use Getopt::Std;
+getopts('dh');
+
+usage() if $opt_h;
+$debug = 1 if defined($opt_d);
+print STDERR "DEBUG: Enabling debug mode ($debug)\n" if $debug;
+$|=1 if $debug;
+
+my $filename;
+$filename = $ARGV[0];
+if ( $opt_h or ! defined($filename) or $filename eq "")
+{
+    print STDERR "ERROR: Missing filename argument\n";
+    usage();
+}
+print STDERR "DEBUG: Reading $filename\n" if $debug;
 open IN, $filename or die "Cannot open $filename : $!";
 
 $invendor = 0;
@@ -61,38 +91,45 @@ $vendorname = "";
 $vendorurl = "";
 $vendordeburl = "";
 $vendoremail = "";
-while (defined($line = <IN>)) {
-    if ($invendor) {  # We are in a vendor entry
-        if ($line =~ /^[\t ]*<vendor ([^>]+)>/) {
+my $numline = 0;
+while ($line = <IN>) {
+        chomp $line;
+        $numline++;
+        print STDERR "DEBUG: Reading line $numline\n" if $debug > 1;
+
+        next if $line =~ /^\#/ ; # Skip comments
+
+        if ($line =~ /^\s*<vendor name="([^>]+)"/) {
             $vendorname = $1;
+            $invendor = 1;
+            print STDERR "DEBUG: Found vendor $vendorname ($filename: $numline)\n" if $debug;
         }
-        if ($line =~ /^[\t ]*<URL ([^>]+)>/) {
+     if ($invendor) {  # We are in a vendor entry
+        if ($line =~ /^[\t ]*url="([^>]+)"/) {
             $vendorurl = $1;
+            print STDERR "DEBUG: Found URL $vendorurl ($filename: $numline)\n" if $debug;
         }
-        if ($line =~ /^[\t ]*<URLdeb ([^>]+)>/) {
+        if ($line =~ /^[\t ]*deburl="([^>]+)"/) {
             $vendordeb = $1;
+            print STDERR "DEBUG: Found Debian URL $vendordeb ($filename: $numline)\n" if $debug;
         }
-        if ($line =~ /^[\t ]*<email ([^>]+)>/) {
+        if ($line =~ /^[\t ]*contacturl="mailto:([^>]+)"/) {
             $vendoremail = $1;
+            print STDERR "DEBUG: Found email $vendoremail ($filename: $numline)\n" if $debug;
         }
-        if ($line =~ /^[\t ]*<\/vendorentry>/) {
+        if ($line =~ /\/>/) {
+            print STDERR "DEBUG: Testing vendor vendorname ($filename: $numline)\n" if $debug;
             test_vendor($vendorname, $vendorurl, $vendordeb,$vendoremail);
-            $invendor = 0;
             $vendorname = "";
             $vendorurl = "";
             $vendordeburl = "";
             $vendoremail = "";
+            $invendor = 0;
         }
-        if ($line =~ /^[\t ]*<vendorentry>/) {
-            die "Unexpected opening vendorentry tag";
+        if ($line =~ /<country/ ){
+          die "Unexpected closing of vendor tag in line $numline";
         }
-    } else {
-        if ($line =~ /^[\t ]*<vendorentry>/) {
-            $invendor = 1;
-        }
-        if ($line =~ /^[\t ]*<\/vendorentry>/) {
-            die "Unexpected  closing vendorentry tag";
-        }
+
     }
 }
 
